c7f4554d8ba5930ac284bcbbdf1a008645a45a34
bchen1024 <bchen_1024@126.com>
2019-10-11 23:51:13 +0800

    监控服务

---
+++ jf-cloud/jf-cloud-monitor/src/main/java/com/btsoft/jf/cloud/monitor/JfCloudMonitorApplication.java
@@ -0,0 +1,18 @@
+package com.btsoft.jf.cloud.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * JF Cloud 监控启动类
+ * @author jeo_cb
+ * @date 2019/10/11
+ **/
+@SpringBootApplication
+public class JfCloudMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfCloudMonitorApplication.class, args);
+	}
+
+}
