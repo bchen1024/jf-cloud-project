63380a9a67c6d47c9f95e464f50b2729cc943794
bchen1024 <bchen_1024@126.com>
2019-11-09 08:44:02 +0800

    用户状态枚举调整

---
+++ jf-cloud/jf-cloud-platform/src/main/java/com/btsoft/jf/cloud/platform/config/basic/dto/post/PostQueryDTO.java
@@ -0,0 +1,11 @@
+package com.btsoft.jf.cloud.platform.config.basic.dto.post;
+
+import com.btsoft.jf.cloud.core.base.dto.impl.SearchKeywordDTO;
+
+/**
+ * 职位查询DTO
+ * @author jeo_cb
+ * @date 2019/11/8
+ **/
+public class PostQueryDTO extends SearchKeywordDTO {
+}
